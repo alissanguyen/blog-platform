67c7ca14edcb8d3ac8359cade61ece08dbc717f2
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-04-20 17:09:59 -0700

    Clean up and add constants

---
+++ app/constants.ts
@@ -0,0 +1,84 @@
+export const defaultLayoutClassname = "max-w-screen-2xl m-auto px-10 2xl:px-0"
+export const fixedWidthContentClassname = "max-w-screen-lg m-auto"
+
+export const footerLinks = [
+    {
+        id: "help",
+        label: "Help",
+        link: "/help"
+    },
+    {
+        id: "writers",
+        label: "Writers",
+        link: "/writers"
+    },
+    {
+        id: "privacy",
+        label: "Privacy",
+        link: "/privacy"
+    },
+    {
+        id: "about",
+        label: "About",
+        link: "/about"
+    },
+    {
+        id: "terms",
+        label: "Terms",
+        link: "/terms"
+    },
+    {
+        id: "blog",
+        label: "Blog",
+        link: "/blog"
+    },
+    {
+        id: "contact",
+        label: "Contact",
+        link: "/contact"
+    }
+]
+
+export const filteredBy = [
+    {
+        id: "topposts",
+        label: "Top Posts",
+        link: "/topposts"
+        // Should add filtered by week, month, year, infinity
+    },
+    {   
+        id: "latest",
+        label: "Latest",
+        link: "/latest"
+    },
+    {
+        id: "followedtopics",
+        label: "Followed Topics",
+        link: "/followedtopics"
+    }
+]
+
+export const leftNavigationMenuLinks = [
+    {
+        id: "home",
+        label: "Home",
+        link: "/home",
+
+    },
+    {
+        id: "notifications",
+        label: "Notifications",
+        link: "/notifications",
+    },
+    {
+        id: "bookmarks",
+        label: "Bookmarks",
+        link: "/bookmarks",
+    },
+    // QUESTION: Should we add this to the left menu?
+    // {
+    //     id: "write",
+    //     label: "Write",
+    //     link: "/write",
+    // },
+]
