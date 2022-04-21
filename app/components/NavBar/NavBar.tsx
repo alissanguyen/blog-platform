ef35581d51b2741c9adec50ec0348e3414a59d9c
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-04-20 18:21:37 -0700

    Change navbar padding

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -10,7 +10,7 @@ export const links: LinksFunction = () => {
 };
 
 const NavBar: React.FC<Props> = (props) => {
-  return <div className={`NavBar__Wrapper py-10 ${defaultLayoutClassname} flex flex-row items-center`}>
+  return <div className={`NavBar__Wrapper pt-10 ${defaultLayoutClassname} flex flex-row items-center`}>
     {/* TODO: Update app name */}
     <span className="uppercase font-medium text-5xl">Logo Name</span>
     <div>
