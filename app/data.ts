eafbe24c8687d8fcacf6a8e74b5df08ace7f05a0
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-04-20 18:20:27 -0700

    Add sample data

---
+++ app/data.ts
@@ -0,0 +1,169 @@
+export const topTrendingSampleData = [
+    {
+    id: "A335C48D-ED05-A10D-670A-F2E9AB7E8697",
+    title: "Donec tempus, lorem fringilla ornare placerat, orci",
+    author: "Cody Mcdonald",
+    date: "Jun 3, 2022",
+    readingTime: "10",
+	avatarUrl: "https://pickaface.net/gallery/avatar/20160625_050020_889_FAKE.png"
+},
+{
+    id: "4A7AAB89-1394-219B-9056-341ACE7DA358",
+    title: "pede, nonummy ut, molestie in, tempus eu,",
+    author: "Honorato Bryant",
+    date: "Dec 27, 2021",
+    readingTime: "19"
+	,avatarUrl: "https://pickaface.net/gallery/avatar/20120409_230759_3646_Fake.png"
+},
+{
+    id: "70F1D0E5-D79A-1C61-056E-38DA8DD2AA74",
+    title: "sit amet lorem semper auctor. Mauris vel",
+    author: "Aquila Daniels",
+    date: "Aug 22, 2021",
+    readingTime: "6",avatarUrl: "https://pickaface.net/gallery/avatar/20120409_230759_3646_Fake.png"
+},
+{
+    id: "481850C1-5C6C-7C17-2EE3-F7FE4BD7DB23",
+    title: "dictum mi, ac mattis velit justo nec",
+    author: "Rhonda Mcdonald",
+    date: "Dec 18, 2021",
+    readingTime: "13",avatarUrl: "https://pickaface.net/gallery/avatar/unr_anakahfakesmith_181119_2202_v01a4.png"
+},
+{
+    id: "633BE75A-E3F7-4DC5-F2D9-D3B0397B2372",
+    title: "ante ipsum primis in faucibus orci luctus",
+    author: "Stephen Le",
+    date: "Jan 14, 2023",
+    readingTime: "9",avatarUrl: "https://pickaface.net/gallery/avatar/29659976_190208_1931_x7bs7.png"
+},
+{
+    id: "18264E6D-7A2D-F04A-C7A1-1741F0EC1B22",
+    title: "elit. Curabitur sed tortor. Integer aliquam adipiscing",
+    author: "Owen Hogan",
+    date: "Jul 14, 2021",
+    readingTime: "12",avatarUrl: "https://pickaface.net/gallery/avatar/unr_fake_190524_1549_9fgji7.png"
+}]
+
+export const sampleDataInfiniteScroll = [
+    {
+		id: "E54606C5-E7C9-9E8D-70D5-E49E2F5295BA",
+		title: "turpis nec mauris blandit mattis. Cras eget",
+		author: "Quintessa England",
+		date: "Mar 27, 2023",
+		readingTime: "11",
+		excerpt: "nec metus facilisis lorem tristique"
+	},
+	{
+		id: "31D6F9B7-6928-DC81-6D54-1EAE7B9DD668",
+		title: "lorem fringilla ornare placerat, orci lacus vestibulum",
+		author: "Jescie Church",
+		date: "Aug 31, 2021",
+		readingTime: "12",
+		excerpt: "hendrerit a, arcu. Sed et libero. Proin mi. Aliquam gravida mauris ut mi. Duis risus odio, auctor vitae, aliquet"
+	},
+	{
+		id: "18264E6D-7A2D-F04A-C7A1-1741F0EC1B22",
+		title: "elit. Curabitur sed tortor. Integer aliquam adipiscing",
+		author: "Owen Hogan",
+		date: "Jul 14, 2021",
+		readingTime: "12",
+		excerpt: "erat vitae risus. Duis a mi fringilla"
+	},
+	{
+		id: "8E7724EA-9C19-16B3-5098-8B425CA7E5C5",
+		title: "ornare egestas ligula. Nullam feugiat placerat velit.",
+		author: "Anastasia Rowland",
+		date: "Aug 22, 2021",
+		readingTime: "18",
+		excerpt: "nisi a odio semper cursus."
+	},
+	{
+		id: "75B99555-9F7C-EC10-2689-DAB124255ED3",
+		title: "cubilia Curae Phasellus ornare. Fusce mollis. Duis",
+		author: "Macey Pratt",
+		date: "Aug 17, 2022",
+		readingTime: "12",
+		excerpt: "id magna et ipsum cursus vestibulum. Mauris magna. Duis"
+	},
+    {
+		id: "C286A274-E0EB-6D1D-7156-D82AB2B5C0A2",
+		title: "lorem, auctor quis, tristique ac, eleifend vitae,",
+		author: "Samson Herring",
+		date: "Oct 6, 2022",
+		readingTime: "11",
+		excerpt: "quis diam. Pellentesque habitant morbi tristique senectus et netus et"
+	},
+	{
+		id: "2122606A-28AC-FD18-151A-D1A9A3CF8EC1",
+		title: "mi felis, adipiscing fringilla, porttitor vulputate, posuere",
+		author: "Rafael Compton",
+		date: "Nov 11, 2022",
+		readingTime: "7",
+		excerpt: "vitae, erat. Vivamus nisi. Mauris nulla. Integer urna. Vivamus molestie"
+	},
+	{
+		id: "ECBB4639-385E-A6C4-A3B2-92C09E5C25F9",
+		title: "interdum enim non nisi. Aenean eget metus.",
+		author: "Sasha Woodard",
+		date: "Mar 3, 2022",
+		readingTime: "7",
+		excerpt: "Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus."
+	},
+	{
+		id: "8CD58131-6858-D57E-A21C-12221E189465",
+		title: "ut nisi a odio semper cursus. Integer",
+		author: "Lynn Moreno",
+		date: "Jun 4, 2022",
+		readingTime: "15",
+		excerpt: "Quisque porttitor eros nec tellus. Nunc lectus pede, ultrices"
+	},
+	{
+		id: "90564C6C-64A5-9B75-2776-60454AD4AAF1",
+		title: "malesuada vel, convallis in, cursus et, eros.",
+		author: "John Fuller",
+		date: "Mar 8, 2021",
+		readingTime: "18",
+		excerpt: "purus. Nullam scelerisque neque sed sem egestas blandit. Nam nulla"
+	},
+    {
+		id: "5EFA3D49-11CC-C924-7814-5445788BD144",
+		title: "elit, a feugiat tellus lorem eu metus.",
+		author: "Alden Acosta",
+		date: "Jan 15, 2022",
+		readingTime: "14",
+		excerpt: "interdum enim non nisi. Aenean eget metus. In nec orci. Donec"
+	},
+	{
+		id: "6B962142-4AAE-D15C-CF1A-58697C99B2ED",
+		title: "in aliquet lobortis, nisi nibh lacinia orci,",
+		author: "Fulton Rivera",
+		date: "Jul 2, 2022",
+		readingTime: "13",
+		excerpt: "sit amet, dapibus id, blandit at, nisi. Cum sociis natoque"
+	},
+	{
+		id: "EC198B0D-B268-2694-1260-FB61A3ACA281",
+		title: "Suspendisse aliquet, sem ut cursus luctus, ipsum",
+		author: "Rafael Knapp",
+		date: "Oct 13, 2021",
+		readingTime: "14",
+		excerpt: "adipiscing, enim mi tempor lorem, eget mollis lectus pede et"
+	},
+	{
+		id: "9CAE7503-19DA-0274-1670-3A33208AC9B9",
+		title: "mauris a nunc. In at pede. Cras",
+		author: "Damon Terry",
+		date: "Jan 10, 2022",
+		readingTime: "17",
+		excerpt: "sem magna nec quam. Curabitur vel lectus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus"
+	},
+	{
+		id: "E3788C46-6CF3-7614-DA3B-7C329B912112",
+		title: "gravida mauris ut mi. Duis risus odio,",
+		author: "Cain Beasley",
+		date: "Feb 17, 2022",
+		readingTime: "17",
+		excerpt: "fames ac turpis egestas. Aliquam fringilla cursus purus. Nullam scelerisque neque sed sem egestas blandit. Nam nulla"
+	}
+]
+
