f9d0caf3fef4a996c87ca44ff3668d7f4fed4d11
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-04-20 18:21:14 -0700

    Add types

---
+++ app/types.ts
@@ -0,0 +1,20 @@
+export enum SupportedTheme {
+    DARK = "dark",
+    LIGHT = "light"
+  }
+
+  export enum ContactFormFields {
+    email = "email",
+    name = "name",
+    subject = "subject",
+    message = "message"
+  }
+
+//   NOTE: Can modify this to add/remove type of alert
+  export enum AlertType {
+    ERROR = "error",
+    SUCCESS = "success",
+    CONFIRMED = "confirmed"
+  }
+
+  
