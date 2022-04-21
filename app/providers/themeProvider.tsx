33d9936de96163c61c982b1df5e863782cedcad9
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-04-20 18:21:49 -0700

    Add theme provider

---
+++ app/providers/themeProvider.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import type { SupportedTheme } from "~/types";
+
+export type ThemeContextValue = {
+  theme: SupportedTheme;
+  updateTheme: (newTheme: SupportedTheme) => void;
+};
+
+const ThemeContext = React.createContext<ThemeContextValue | undefined>(
+  undefined
+);
+
+export const useTheme = (): ThemeContextValue => {
+  const contextValue = React.useContext(ThemeContext);
+
+  if (!contextValue) {
+    throw new Error(
+      "You are trying to use useGlobalContext without rendering a ThemeContext.Provider somewhere above this component in the component tree. Render a ThemeContext.Provider somewhere above this component in the component tree to resolve this issue."
+    );
+  }
+
+  return contextValue;
+};
+
+interface ThemeContextProviderProps {
+  initialTheme: SupportedTheme;
+}
+
+export const ThemeContextProvider: React.FC<ThemeContextProviderProps> = (
+  props
+) => {
+  const [theme, setTheme] = React.useState<SupportedTheme>(props.initialTheme);
+
+  const updateTheme = (newTheme: SupportedTheme) => {
+    setTheme(newTheme);
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        updateTheme
+      }}
+    >
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
